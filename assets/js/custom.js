e695aeca4849cbb4a65d2235f001e4c8b4d60956
rajcodetrinity <raj.codetrinity@gmail.com>
2022-05-06 18:42:18 -1200

    design popup and update design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,25 @@ $(document).ready(function () {
 $(document).on('click', function () {
   $('.collapse').collapse('hide');
 });
+
+
+
+
+
+$('.product-main-image').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  asNavFor: '.product-thumb-image'
+});
+$('.product-thumb-image').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: '.product-main-image',
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true
+});
